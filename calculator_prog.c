3b8a53d1dd41b5a2fd842c0164a5f291a8c3425c
nityanandkumar <kumar610nityanand@gmail.com>
2022-07-27 11:57:43 +0530

    commiting subtraction operation

--- calculator_prog.c
+++ calculator_prog.c
@@ -5,6 +5,11 @@ void addition(long int a, long int b)
     printf("the value after adding the two given numbers is: %ld", a + b);
 }
 
+void subtraction(long int num1,long int num2)
+{
+    printf("the value after subtracting the two given number is: %ld", num1 - num2);
+}
+
 int main()
 {
     long int num1;
@@ -14,12 +19,16 @@ int main()
     printf("please Enter num2:\n");
     scanf("%ld", &num2);
     int to_do;
-    printf("please enter 1 for addition \n");
+    printf("please enter \n 1 for addition \n 2 for subtraction\n ");
     scanf("%d", &to_do);
     switch (to_do)
     {
     case 1:
         addition(num1 , num2);
+        break;
+    
+    case 2:
+        subtraction(num1 , num2);
         break;
     
     default:
